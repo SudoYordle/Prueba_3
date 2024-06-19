5183d392009b9876d9b52235a0d3a96aa29f1113
SudoYordle <cri.collao@duocuc.cl>
2024-06-19 16:00:04 -0400

    Add files via upload

---
+++ PaquExpress.py
@@ -0,0 +1,16 @@
+#PaquExpress
+
+def registro():
+    listaDatos={"cliente":"","direccion":"","sector":"","detalle":{"pequeño":0,"mediano":0,"grande":0}}
+    
+
+
+
+
+
+
+#Menu
+selectOpcion=0
+while selectOpcion!=4:
+    if selectOpcion==1:
+        
